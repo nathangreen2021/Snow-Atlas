3430dbc8e0c0acbeb42750231b255eba496f671f
Nathan_Green <90873678+nathangreen2021@users.noreply.github.com>
2022-05-09 04:13:55 +0000

    Add files via upload

---
+++ ConfigureYourSnowExtender.ps1
@@ -0,0 +1,17 @@
+﻿################## Enter the following details to create 
+
+$resourceGroupName = "SnowSoftware"
+$location = "southeastasia"
+$adminUsername = "admin"
+$adminPassword = Read-Host -Prompt "Enter the administrator password" -AsSecureString
+$dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
+
+New-AzResourceGroup -Name $resourceGroupName -Location "$location"
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateUri "https://raw.githubusercontent.com/nathangreen2021/Snow-Atlas/main/SnowExtender.json" `
+    -adminUsername $adminUsername `
+    -adminPassword $adminPassword `
+    -dnsLabelPrefix $dnsLabelPrefix
+
+ (Get-AzVm -ResourceGroupName $resourceGroupName).name
